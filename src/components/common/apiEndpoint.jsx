1ab6bc288e3ea5d89f803d069d431c58b1b9e6b7
Jganyo-dotcom <elikemejay@gmail.com>
2026-08-09 04:00:07 +0000

    added apis

--- src/components/common/apiEndpoint.jsx
+++ src/components/common/apiEndpoint.jsx
@@ -1,2 +1,2 @@
-export const baseApi = "http://localhost:5000";
-//export const baseApi = "https://ecommercebackend-5b08.onrender.com";
+//export const baseApi = "http://localhost:5000";
+export const baseApi = "https://ecommercebackend-5b08.onrender.com";
